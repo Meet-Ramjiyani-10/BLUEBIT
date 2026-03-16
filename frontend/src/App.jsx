00d4b3b051cbd9bd636dbecbd63e4d3d2c0a5b05
sohammulik02 <sohamvijaymulik222@gmail.com>
2026-03-16 13:08:22 +0530

    changes in ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/analyzer" element={<Analyzer />} />
-      </Routes>
+      </Routes> 
     </BrowserRouter>
   );
 }
